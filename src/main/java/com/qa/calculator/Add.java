55505e2387e24e57ae09bf152d811adc82f397ed
Sharm16 <shyam-s@live.co.uk>
2018-07-11 11:12:07 +0100

    refactored

---
+++ src/main/java/com/qa/calculator/Add.java
@@ -0,0 +1,7 @@
+package com.qa.calculator;
+
+public class Add {
+	public int add(int num1, int num2) {
+		return num1 + num2;
+	}
+}
--- src/test/java/com/qa/calculator/CalculatorTest.java
+++ src/test/java/com/qa/calculator/CalculatorTest.java
@@ -7,26 +7,26 @@ public class CalculatorTest {
 
 	@Test
 	public void addtest() {
-		Calculator calc = new Calculator();
-		int actualValue = calc.add(2, 2);
+		Add a = new Add();
+		int actualValue = a.add(2, 2);
 		Assert.assertEquals(4, actualValue);
 	}
 	@Test
 	public void subtracttest() {
-		Calculator calc = new Calculator();
-		int actualValue = calc.subtract(2, 2);
+		Subtract s = new Subtract();
+		int actualValue = s.subtract(2, 2);
 		Assert.assertEquals(0, actualValue);
 	}
 	@Test
 	public void multiplytest() {
-		Calculator calc = new Calculator();
-		int actualValue = calc.multiply(2, 2);
+		Multiply m =new Multiply();
+		int actualValue = m.multiply(2, 2);
 		Assert.assertEquals(4, actualValue);
 	}
 	@Test
 	public void dividetest() {
-		Calculator calc = new Calculator();
-		int actualValue = calc.divide(0, 2);
+		Divide d = new Divide();
+		int actualValue = d.divide(0, 2);
 		Assert.assertEquals(0, actualValue);
 	}
 
