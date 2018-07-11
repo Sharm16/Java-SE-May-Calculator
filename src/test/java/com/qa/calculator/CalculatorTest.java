583586e401d32ec410d072e230dc5f2f9eb4ce27
Sharm16 <shyam-s@live.co.uk>
2018-07-11 11:04:16 +0100

    calculator refactor

--- src/test/java/com/qa/calculator/CalculatorTest.java
+++ src/test/java/com/qa/calculator/CalculatorTest.java
@@ -6,10 +6,29 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	@Test
-	public void test() {
+	public void addtest() {
 		Calculator calc = new Calculator();
 		int actualValue = calc.add(2, 2);
 		Assert.assertEquals(4, actualValue);
 	}
+	@Test
+	public void subtracttest() {
+		Calculator calc = new Calculator();
+		int actualValue = calc.subtract(2, 2);
+		Assert.assertEquals(0, actualValue);
+	}
+	@Test
+	public void multiplytest() {
+		Calculator calc = new Calculator();
+		int actualValue = calc.multiply(2, 2);
+		Assert.assertEquals(4, actualValue);
+	}
+	@Test
+	public void dividetest() {
+		Calculator calc = new Calculator();
+		int actualValue = calc.divide(0, 2);
+		Assert.assertEquals(0, actualValue);
+	}
 
+	
 }
