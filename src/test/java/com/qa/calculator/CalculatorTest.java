164ca647c1c81e85fe4ef65ef4429f7f642f5948
Sharm16 <shyam-s@live.co.uk>
2018-07-11 11:05:38 +0100

    red

--- src/test/java/com/qa/calculator/CalculatorTest.java
+++ src/test/java/com/qa/calculator/CalculatorTest.java
@@ -11,24 +11,7 @@ public class CalculatorTest {
 		int actualValue = calc.add(2, 2);
 		Assert.assertEquals(4, actualValue);
 	}
-	@Test
-	public void subtracttest() {
-		Calculator calc = new Calculator();
-		int actualValue = calc.subtract(2, 2);
-		Assert.assertEquals(0, actualValue);
-	}
-	@Test
-	public void multiplytest() {
-		Calculator calc = new Calculator();
-		int actualValue = calc.multiply(2, 2);
-		Assert.assertEquals(4, actualValue);
-	}
-	@Test
-	public void dividetest() {
-		Calculator calc = new Calculator();
-		int actualValue = calc.divide(0, 2);
-		Assert.assertEquals(0, actualValue);
-	}
+
 
 	
 }
